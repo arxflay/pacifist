eab9e1fe74826084300107826922cef1c3b9bf0b
arxflay <arxflay@yandex.ru>
2024-03-27 21:02:07 +0100

    Configure to make deb packages

--- cmake/Findarxengine.cmake
+++ cmake/Findarxengine.cmake
@@ -1,18 +1,21 @@
+set(ARXENGINE_FOUND FALSE)
+
 find_path(ARXENGINE_INCLUDE_DIR arxengine/GameApp.h)
 mark_as_advanced(ARXENGINE_INCLUDE_DIR) #mark as non visible in gui
 find_library(ARXENGINE_LIBRARY NAMES arxengine arxengined)
 mark_as_advanced(ARXENGINE_LIBRARY)
 
-message(STATUS ${ARXENGINE_INCLUDE_DIR})
 
 include(FindPackageHandleStandardArgs)
 find_package_handle_standard_args(arxengine
     FOUND_VAR ARXENGINE_FOUND
-    REQUIRED_VARS LIBARXENGINE_LIBRARY LIBARXENGINE_INCLUDE_DIR)
+    REQUIRED_VARS ARXENGINE_LIBRARY ARXENGINE_INCLUDE_DIR)
 
-if(ARXENGINE_FOUND)
+if(${ARXENGINE_FOUND})
     add_library(arxengine UNKNOWN IMPORTED)
     #set headers and lib
+    set_property(TARGET arxengine APPEND PROPERTY
+        IMPORTED_CONFIGURATIONS RELEASE)
     set_target_properties(
       arxengine PROPERTIES
       INTERFACE_INCLUDE_DIRECTORIES "${ARXENGINE_INCLUDE_DIR}"
